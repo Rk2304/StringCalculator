9e1258d2b544a69d0a05a673afae5b3e497d800b
Rk2304 <rishabh23kothari@gmail.com>
2021-10-17 19:46:12 +0530

    first commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,22 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        int a=0;
+        
+        char[] arr=input.toCharArray();
+        int[] b=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+          if(arr[i]!=',')
+          {
+              b[i]=arr[i];
+          }
+        }
+        for(int i=0;i<b.length;i++){
+          if(b[i]>0){
+              a=a+(b[i]-48);
+          }    
+        }
+        return a;
     }
 
 }
