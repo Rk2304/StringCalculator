ba93b59015955ce0d00da7deddc2fd663d607dcb
Rk2304 <rishabh23kothari@gmail.com>
2021-10-17 19:49:15 +0530

    second commit

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,7 +8,7 @@ class StringCalculator {
         char[] arr=input.toCharArray();
         int[] b=new int[arr.length];
         for(int i=0;i<arr.length;i++){
-          if(arr[i]!=',')
+          if(arr[i]!=',' && arr[i]!='\n')
           {
               b[i]=arr[i];
           }
