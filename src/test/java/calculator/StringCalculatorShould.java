531fb882477b25f63aa413d9ad7847f3c90c9b92
Rk2304 <rishabh23kothari@gmail.com>
2021-10-18 09:22:09 +0530

    fourth commit

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,11 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+
+    @Test
+    void string_with_delimiters() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(5, stringCalculator.add("1\n2;3"));
+    }
+
 }
